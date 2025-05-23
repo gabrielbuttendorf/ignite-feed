c56ad7aeecc50e91a08361f0ec808a62920a6e30
Gabriel <bielbuttendorf@gmail.com>
2025-05-23 18:55:38 -0300

    css modules first use

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Gabriel Buttendorf"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut id quam ut veniam vitae nostrum eos modi? Sint tenetur expedita fugit consectetur delectus voluptatem, ut, aut odio, quis omnis totam."
