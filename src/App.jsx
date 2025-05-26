68df728802b7a16e21ca06bc58c29b0e1769e01b
Gabriel <bielbuttendorf@gmail.com>
2025-05-26 20:54:09 -0300

    sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Gabriel Buttendorf"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut id quam ut veniam vitae nostrum eos modi? Sint tenetur expedita fugit consectetur delectus voluptatem, ut, aut odio, quis omnis totam."
-      />
-      <Post
-        author="Ana Buttendorf"
-        content="Esse é o parágrafo escrito pela Ana Luisa."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gabriel Buttendorf"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut id quam ut veniam vitae nostrum eos modi? Sint tenetur expedita fugit consectetur delectus voluptatem, ut, aut odio, quis omnis totam."
+          />
+          <Post
+            author="Ana Buttendorf"
+            content="Esse é o parágrafo escrito pela Ana Luisa."
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilSimpleLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1595044426077-d36d9236d54a?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/gabrielbuttendorf.png"
+        />
+        <strong>Gabriel Buttendorf</strong>
+        <span>Frontend Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
