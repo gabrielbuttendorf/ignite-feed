90e570ef696e4c1ecf83e72676c3ca955cefb31a
Gabriel <bielbuttendorf@gmail.com>
2025-06-05 19:44:40 -0300

    add comment sections

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gabrielbuttendorf.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Buttendorf</strong>
+              <time title="24 de Maio às 21:20" datetime="2025-05-24 21:20:15">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
